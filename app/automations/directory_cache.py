39d8d723e616a37db02b547002141a0231709609
Akashdeep Singh <aksinghsidhu07@gmail.com>
2026-08-31 19:24:04 +0530

    Add cached entry lookup

--- app/automations/directory_cache.py
+++ app/automations/directory_cache.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
+from typing import Optional
 from .file_tools import validate_directory, get_modification_time, build_directory_tree
 
 db = SQLAlchemy()
@@ -11,7 +13,7 @@ class DirectorySizeCache(db.Model):
     full_path: Mapped[str] = mapped_column(primary_key = True)
     name: Mapped[str]
     size: Mapped[int]
-    mtime: Mapped[datetime]
+    mtime: Mapped[Optional[datetime]]
     is_dir: Mapped[bool]
     parent_path: Mapped[str]
 
@@ -23,7 +25,7 @@ def traverse_directory_tree(tree, current_path, results):
                 "full_path": str(current_path / content["name"]),
                 "size": content["size"],
                 "name": content["name"],
-                "mtime": get_modification_time(current_path / content["name"]),
+                "mtime": None,
                 "is_dir": True,
                 "parent_path": str(current_path)
             })
@@ -47,7 +49,7 @@ def build_cache(path):
         "full_path": str(directory),
         "name": directory.name,
         "size": directory_tree["size"],
-        "mtime": get_modification_time(directory),
+        "mtime": None,
         "is_dir": True,
         "parent_path": str(directory.parent)
     }]
@@ -65,3 +67,15 @@ def build_cache(path):
         )
         db.session.add(cache)
     db.session.commit()
+
+def get_cached_entry(path):
+    path = str(path)
+    result = db.session.execute(
+        select(DirectorySizeCache).where(
+            DirectorySizeCache.full_path == path
+        )
+    )
+
+    cache = result.scalar_one_or_none()
+
+    return cache
